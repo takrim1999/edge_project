3aaf4eca6f4e6a9f71ad85d40651d49fee47f713
takrim1999 <takrimulalom@gmail.com>
2024-11-25 18:47:03 +0600

    Done for Day4
    Added some personal programs, better versions of exercises

---
+++ Day4/exercise1.py
@@ -0,0 +1,8 @@
+age1 = int(input("Age of person1: "))
+age2 = int(input("Age of person2: "))
+if age1>age2:
+    print("Person1 elder, Age: ", age1)
+elif age1<age2:
+    print("Person2 elder, Age: ", age2)
+else:
+    print("Both Equal!")
